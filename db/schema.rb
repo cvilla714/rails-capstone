9cf7da0801baac705bd39bb1754c708d0e4292b3
Cosmel Villalobos <cosmelvg@gmail.com>
2020-12-21 17:52:52 -0500

    added the likes table and created the relantionship between the user and article table with the likes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_21_211443) do
+ActiveRecord::Schema.define(version: 2020_12_21_222852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,15 @@ ActiveRecord::Schema.define(version: 2020_12_21_211443) do
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.bigint "article_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_likes_on_article_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "password", null: false
@@ -40,4 +49,6 @@ ActiveRecord::Schema.define(version: 2020_12_21_211443) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "likes", "articles"
+  add_foreign_key "likes", "users"
 end
