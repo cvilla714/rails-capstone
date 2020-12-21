b5ae840a0cfe31323b23f40ea2fb2bfbf312c0b9
Cosmel Villalobos <cosmelvg@gmail.com>
2020-12-21 13:32:22 -0500

    added custome route names for devis , created the simple login using only the name and email for registraion and email only for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,path: '',path_names: { sign_up: 'register', sign_in: 'login', sign_out: 'logout'}
   resources :articles
   root to: "articles#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
