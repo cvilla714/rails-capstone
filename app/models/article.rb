aae13d170b9c9ee18eecda9635eb17a16f2d6de8
Cosmel Villalobos <cosmelvg@gmail.com>
2020-12-22 20:07:12 -0500

    added the has many relantion between aricle and article_category

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,7 @@
 class Article < ApplicationRecord
-    belongs_to :user
+  belongs_to :user
+  has_many :likes, dependent: :destroy
+
+  has_many :article_categories, dependent: :destroy
+  has_many :categories, through: :article_categories, source: :category
 end
