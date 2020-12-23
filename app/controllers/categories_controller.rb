9c75264f9bccde3a349aa86b6d15b6899a7a1afc
Cosmel Villalobos <cosmelvg@gmail.com>
2020-12-23 18:04:45 -0500

    authenticated the user before seen the categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: %i[show edit update destroy]
+  before_action :authenticate_user!
 
   # GET /categories
   # GET /categories.json
