a680ca4266e1066f9bee68522ef70169d6019807
Cosmel Villalobos <cosmelvg@gmail.com>
2020-12-23 11:41:33 -0500

    changed to belongs to class name User

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
-  belongs_to :user
-  # belongs_to :author, class_name: 'User'
+  # belongs_to :user
+  belongs_to :author, class_name: 'User'
   has_many :likes, dependent: :destroy
 
   has_many :article_categories, dependent: :destroy
