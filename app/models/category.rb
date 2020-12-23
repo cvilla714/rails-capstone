f7f5679f2c32100c26d191af353eb97f3de7b20d
Cosmel Villalobos <cosmelvg@gmail.com>
2020-12-23 15:08:37 -0500

    added validation for the category

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,8 @@ class Category < ApplicationRecord
   has_many :article_categories
   has_many :articles, through: :article_categories, source: :article
 
-
+  validates :name,:priority,presence: true
+  validates :name,uniqueness: true
+  validates :name, length: { in: 6..29 }
   
 end
