b2e85aa23db15db6dff412241a82feb98c4d28da
Cosmel Villalobos <cosmelvg@gmail.com>
2020-12-23 21:14:31 -0500

    added user authentication to likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,6 @@
 class LikesController < ApplicationController
+  before_action :authenticate_user!, only: %i[create destroy]
+  
   def create
     @like = current_user.likes.new(article_id: params[:article_id])
 
