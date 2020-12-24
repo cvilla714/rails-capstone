6d2cc3ec6d5abf2934b16df934ef3d98c2a2377b
Cosmel Villalobos <cosmelvg@gmail.com>
2020-12-23 23:16:01 -0500

    added the method to get the most recent articles and limit it by 1

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,8 +2,11 @@ class Category < ApplicationRecord
   has_many :article_categories
   has_many :articles, through: :article_categories, source: :article
 
-  validates :name,:priority,presence: true
-  validates :name,uniqueness: true
+  validates :name, :priority, presence: true
+  validates :name, uniqueness: true
   validates :name, length: { in: 6..29 }
-  
+
+  def most_recent_article
+    articles.most_recents.limit(1)
+  end
 end
