0fb34d6a54af8950ac159e1c86646b614df5f23a
Cosmel Villalobos <cosmelvg@gmail.com>
2020-12-24 01:39:32 -0500

    modified the environment.js to use webpack to use jquery and poppes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,18 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
 
+import "bootstrap";
+import "../stylesheets/application";
+
+document.addEventListener("turbolinks:load", () => {
+  $('[data-toggle="tooltip"]').tooltip();
+  $('[data-toggle="popover"]').popover();
+});
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
