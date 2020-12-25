838072dd60b0749d22be0f6eff3d843c964c198d
Cosmel Villalobos <cosmelvg@gmail.com>
2020-12-25 04:31:20 -0500

    modified the file to use fontawesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("channels");
 
 import "bootstrap";
 import "../stylesheets/application";
+import "@fortawesome/fontawesome-free/js/all";
 
 document.addEventListener("turbolinks:load", () => {
   $('[data-toggle="tooltip"]').tooltip();
