840a86067605ea1d7b72083e3481ea1e46269560
Cosmel Villalobos <cosmelvg@gmail.com>
2020-12-25 17:08:09 -0500

    deleted commented out code

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,8 +5,8 @@ class Article < ApplicationRecord
   has_many :article_categories, dependent: :destroy
   has_many :categories, through: :article_categories, source: :category
 
-  validates :title, length: { in: 6..29 }
-  validates :body, length: { maximum: 500 }
+  validates :title, length: { in: 6..100 }
+  validates :body, length: { maximum: 5000}
   validates :title, :body, presence: true
   validates :title, uniqueness: true
 
