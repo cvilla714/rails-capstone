1048ab4f2eba4ddd8e702f3cdf1e671d027a1443
Cosmel Villalobos <cosmelvg@gmail.com>
2020-12-26 16:22:45 -0500

    added rspec and test for the project

---
+++ spec/like_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Like, type: :model do
+    describe 'validation' do
+        it 'should not create the like' do
+         expect(Like.create(article_id:1)).to_not be_valid
+      end
+    end
+ end
+
+ RSpec.describe Like, type: :model do
+    before(:each) do
+        Article.create(id:1,author_id:1,title:"looking for more",body:"the tenacity")
+      end
+    describe 'validation' do
+        it 'should create the like' do
+         expect(Like.create(article_id:1,user_id:1)).to be_valid
+      end
+    end
+ end
