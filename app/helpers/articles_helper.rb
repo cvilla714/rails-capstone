ea14af9bba50245781cc9c96be1e50fd2520e841
Cosmel Villalobos <cosmelvg@gmail.com>
2020-12-26 02:20:04 -0500

    created the helper method that will help to get each category the title and image

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,25 @@
 module ArticlesHelper
+    def nfl_category
+        a = Category.find(3)
+    end
+
+    def nba_category
+        b = Category.find(2)
+    end
+
+    def soccer_category
+        c = Category.find(1)
+    end
+
+    def mlb_category
+        d= Category.find(4)
+    end
+
+    def nhl_category
+        e = Category.find(5)
+    end
+
+
+
+
 end
