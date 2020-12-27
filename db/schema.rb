2bdb68c1e553867c432479bbfcbbf27510fa29c1
Cosmel Villalobos <cosmelvg@gmail.com>
2020-12-27 17:28:07 -0500

    added the caterory_id to each article

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_23_153926) do
+ActiveRecord::Schema.define(version: 2020_12_27_215611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2020_12_23_153926) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.integer "category_id"
     t.index ["author_id"], name: "index_articles_on_author_id"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
