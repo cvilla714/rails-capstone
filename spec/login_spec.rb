5b9415ec61cbdad202143f64070c7b80e97711df
Cosmel Villalobos <cosmelvg@gmail.com>
2020-12-27 15:30:51 -0500

    aded the login test using capybara

--- spec/login_spec.rb
+++ spec/login_spec.rb
@@ -1,24 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe 'Home', type: :feature do
-  before :each do
-    User.create(id: 2, name: 'zoro', email: 'zorro@test.com', password: 'password')
-    Category.create(name: 'Soccer News', priority: 1)
-    Article.create(
-      author_id: 1,
-      title: 'moving forward',
-      body: 'making progress',
-      image: 'https://e0.365dm.com/20/03/2048x1152/skysports-arsenal-olympiakos_4944696.jpg'
-    )
-    ArticleCategory.create(article_id: 1, category_id: 2)
-    Like.create(article_id: 1, user_id: 2)
-  end
   describe 'home page' do
     it 'shows the right content' do
-      puts Article.first.title
-      puts Article.first.image
       visit root_path
-      expect(page).to have_content('Articles')
+      expect(page).to have_content('please create a new article')
+    end
+  end
+end
+
+RSpec.describe 'Home', type: :feature do
+  describe 'home page' do
+    User.create(id: 3, email: 'test@test.com', name: 'testing', password: 'password')
+
+    it 'will log in' do
+      visit new_user_session_path
+      fill_in 'Email', with: 'test@test.com'
+      click_on 'Log in'
+      expect(page).to have_content('Signed in successfully')
     end
   end
 end
