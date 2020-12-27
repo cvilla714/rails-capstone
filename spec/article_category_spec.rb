ee8e9dd29bb18e34a6979ebfc1890c944e9a1421
Cosmel Villalobos <cosmelvg@gmail.com>
2020-12-27 17:31:08 -0500

    added the tests for creating an article category and also test when is not created

--- spec/article_category_spec.rb
+++ spec/article_category_spec.rb
@@ -2,8 +2,21 @@ require 'rails_helper'
 
 RSpec.describe ArticleCategory, type: :model do
   describe 'validation' do
-    it 'should not create the category' do
+    it 'should not create the articlecategory' do
       expect(ArticleCategory.create(article_id: 1)).to_not be_valid
+    end
+  end
+end
+
+
+RSpec.describe ArticleCategory, type: :model do
+  describe 'validation' do
+    let(:article){Article.create(id:3,title: 'all the way',body:"making it count",image:"myurl",category_id:2,author_id:3)}
+    let(:category){Category.create(id:5 ,name:"NBA Headlines",priority:7)}
+    it 'should create the articlecategory' do
+      article.save
+      category.save
+      expect(ArticleCategory.create(article_id: 3,category_id:5)).to be_valid
     end
   end
 end
