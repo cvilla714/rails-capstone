7f7b256c10127502c668f2d14357c6984897aea3
Cosmel Villalobos <cosmelvg@gmail.com>
2023-07-07 20:56:57 -0400

    Modify the logic_spec.rb
    - Fix failing tests after upgrade

--- spec/login_spec.rb
+++ spec/login_spec.rb
@@ -1,27 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe 'Login', type: :feature do
-  describe 'Login content' do
+  before do
     User.create(id: 3, email: 'test@test.com', name: 'testing', password: 'password')
+  end
 
+  describe 'Login content' do
     it 'will log in' do
       visit new_user_session_path
       fill_in 'Email', with: 'test@test.com'
-      click_on 'Log in'
+      click_button 'Log in'
       expect(page).to have_content('Signed in successfully')
     end
-  end
-end
 
-RSpec.describe 'Login', type: :feature do
-  describe 'Login content' do
-    User.create(id: 3, email: 'test@test.com', name: 'testing', password: 'password')
-
-    it 'will not  log in' do
+    it 'will not log in' do
       visit new_user_session_path
-      fill_in 'Email', with: 'test@test.com'
-      click_on 'Log in'
-      expect(page).to_not have_content('the record')
+      fill_in 'Email', with: 'unkown@example.com'
+      click_button 'Log in'
+      expect(page).to have_css('.alert.main', text: 'Invalid Email or password')
     end
   end
 end
