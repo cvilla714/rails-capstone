e1d49616b5d42ebdd41d9e452a42f508df7f424a
Cosmel Villalobos <cosmelvg@gmail.com>
2023-07-09 03:55:03 -0400

    Refactored the code
    - To work with ruby version 3.0.6

--- spec/article_category_spec.rb
+++ spec/article_category_spec.rb
@@ -4,16 +4,16 @@ RSpec.describe ArticleCategory, type: :model do
   describe 'validation' do
     let!(:author) { User.create(name: 'John Doe') }
     let!(:article) do
-      Article.create(title: 'all the way', body: 'making it count', image: 'myurl', category_id: 2, author:)
+      Article.create(title: 'all the way', body: 'making it count', image: 'myurl', category_id: 2, author: author)
     end
     let!(:category) { Category.create(name: 'NBA Headlines', priority: 7) }
 
-    it 'should not create the articlecategory' do
-      expect(ArticleCategory.create(article:)).not_to be_valid
+    it 'should not create the articlecategory if category is not provided' do
+      expect(ArticleCategory.create(article: article)).not_to be_valid
     end
 
     it 'should create the articlecategory' do
-      expect(ArticleCategory.create(article:, category:)).to be_valid
+      expect(ArticleCategory.create(article: article, category: category)).to be_valid
     end
   end
 end
