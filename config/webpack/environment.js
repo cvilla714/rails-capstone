7a88ac3f5df56b1e31d5875a83e9505040cba55b
Cosmel Villalobos <cosmelvg@gmail.com>
2023-07-11 20:56:53 -0400

    Fix environment.js
    - Added configuration for nodes

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,24 @@
-const { environment } = require("@rails/webpacker");
-const webpack = require("webpack");
+const { environment } = require('@rails/webpacker');
+const webpack = require('webpack');
+
 environment.plugins.append(
-  "Provide",
+  'Provide',
   new webpack.ProvidePlugin({
-    $: "jquery",
-    jQuery: "jquery",
-    Popper: ["popper.js", "default"],
-  })
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default'],
+  }),
 );
+
+// to fix the issue with node core modules
+environment.config.merge({
+  node: {
+    dgram: 'empty',
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    child_process: 'empty',
+  },
+});
+
 module.exports = environment;
