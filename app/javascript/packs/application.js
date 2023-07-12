484104cd2ff7fc95d2cfb4cff323ebcaf6e0423d
Cosmel Villalobos <cosmelvg@gmail.com>
2023-07-11 20:06:14 -0400

    Modify application.js
    - added complete path for file applicaton.scss

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,10 @@
 import { start as ujsStart } from '@rails/ujs';
 import { start as turbolinksStart } from 'turbolinks';
 import { start as activestorageStart } from '@rails/activestorage';
-import 'channels';
+import '../channels';
 
 import 'bootstrap/dist/js/bootstrap';
-import '../stylesheets/application';
+import '../stylesheets/application.scss';
 import '@fortawesome/fontawesome-free/js/all';
 
 document.addEventListener('turbolinks:load', () => {
